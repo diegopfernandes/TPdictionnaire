ce4022c028a2d0ecf7bbb66f098c39bc9af8f6ba
Diego Fernandes <dieguenho@hotmail.com>
2017-11-16 19:55:12 -0500

    v1

---
+++ Dictionnaire/Add.swift
@@ -0,0 +1,25 @@
+
+
+
+import UIKit
+
+class Add: UIViewController {
+    
+    @IBOutlet weak var fr: UITextField!
+    @IBOutlet weak var en: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    @IBAction func add(_ sender: UIButton) {
+        
+    }
+    
+
+    
+}
+
+
--- Dictionnaire/ViewController.swift
+++ Dictionnaire/ViewController.swift
@@ -1,51 +1,41 @@
 //-------
 import UIKit
 //-------
-class ViewController: UIViewController {
-    //-------
-    // Propriété @IBOutlet représentant un "UITextField" : textField
-    @IBOutlet weak var textField: UITextField!
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // Propriété @IBOutlet représentant un "UITextView" : textView
-    @IBOutlet weak var textView: UITextView!
+    var arrayEnglish: [String]!
+    var arrayFrancais: [String]!
     
-    // Propriété variable de type chain de caracteres (CDC) vide pour afficher le résultat:
-    var resultToDisplay = String()
     
-    // Propriété tableau de type CDC contenant des noms: arrOfNames
-    let arrFra = ["Sapin de Noël", "Dinde", "Bonhomme de neige", "Neige"]
-    let arrEng = ["Christmas Tree", "Turkey", "Snowman"]
-    var dictOfWinter: [String: String]
     
-    // Propriété Propriété de type Dictionnaire pour organiser les noms selon la première lettre
-    var namesByAlphabet = [String.Element: [String]]()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+ 
     
     //-------
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Assigner à "namesByAlphabet" un dictionnaire groupant les noms selon la première lettre
-        namesByAlphabet = Dictionary(grouping: arrOfNames, by: {$0.first!})
+        manageUser()
+ 
     }
-    @IBAction func search(_ sender: UITextField) {
-        //S'assurer que "resultToDisplay" est vide au debut
-        resultToDisplay = ""
-        
-        //Boucle avec les clés et les valeurs de "namesByAlphabet"
-        for (key, value) in namesByAlphabet {
-            
-            //Si le contenu de "textField" égale la valeur de "key" converti en CDC alors:
-            if textField.text == String(key) {
-                
-                // Boucle avec "index" de 0 à plus petit que le nombre d'éléments dans "value"
-                for index in 0..<value.count {
-                    //Assigne à resultToDisplay sa valeur plus l'élément à l'index de "value" et un retour de chariot
-                    resultToDisplay += "\(value[index])\n"
-                }
-            }
+    func manageUser() {
+        if UserDefaults.standard.object(forKey: "french") != nil {
+            arrayFrancais = UserDefaults.standard.object(forKey: "french") as! [String]
+            arrayEnglish = UserDefaults.standard.object(forKey: "english") as! [String]
+        } else {
+            arrayFrancais = [String]()
+            arrayEnglish = [String]()
         }
-        // Assigne au texte de "textView" la valeur de resultToDisplay à la fin de la boucle de "namesByAlphabet"
-        textView.text = resultToDisplay
     }
+
     //-------
 }
