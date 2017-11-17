f33320511221b304002383350b5f9e420f3ff63d
Diego Fernandes <dieguenho@hotmail.com>
2017-11-17 04:36:38 -0500

    v3

--- Dictionnaire/Add.swift
+++ Dictionnaire/Add.swift
@@ -8,18 +8,32 @@ class Add: UIViewController {
     @IBOutlet weak var fr: UITextField!
     @IBOutlet weak var en: UITextField!
     
-    
-    
+    var arrayEnglish: [String]!
+    var arrayFrancais: [String]!
+   //----
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        manageUser()
     }
+    //----
     @IBAction func add(_ sender: UIButton) {
-        
+        arrayEnglish.append(en.text!)
+        arrayFrancais.append(fr.text!)
+        UserDefaults.standard.set(arrayEnglish, forKey: "english")
+        UserDefaults.standard.set(arrayFrancais, forKey: "french")
     }
-    
-
-    
+    //----
+    func manageUser() {
+        if UserDefaults.standard.object(forKey: "french") != nil {
+            arrayFrancais = UserDefaults.standard.object(forKey: "french") as! [String]
+            arrayEnglish = UserDefaults.standard.object(forKey: "english") as! [String]
+        } else {
+            arrayFrancais = [String]()
+            arrayEnglish = [String]()
+        }
+        //----
+    }
+    //----
 }
 
 
